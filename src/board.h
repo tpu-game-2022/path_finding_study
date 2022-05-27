d167eaa19d231790a4aaf81ed149aa1a2a6efd7f
isigurohatune <39hatune.1925@gmail.com>
2022-05-27 22:03:27 +0900

    A*アルゴリズムを実装しました。

--- src/board.h
+++ src/board.h
@@ -1,5 +1,39 @@
 ﻿#pragma once
 #include <iostream>
+#include<vector>
+#include <algorithm>
+
+class Point {
+	int x_ = -1,
+		y_ = -1;
+public:
+	Point(int x, int y) :x_(x), y_(y) {}
+	Point() {}
+	int x() const { return x_; }
+	int y() const { return y_; }
+	void setX(int x) { x_ = x; }
+	void setY(int y) { y_ = y; }
+	void set(int x, int y) { x_ = x; y_ = y; }
+
+	bool operator == (const Point& p) const {
+		return p.x() == x_ && p.y() == y_;
+	}
+	bool operator != (const Point& p) const {
+		return !(p == *this);
+	}
+
+	Point getRight() const { return Point(x_ + 1, y_); }
+	Point getLeft() const { return Point(x_ - 1, y_); }
+	Point getUp() const { return Point(x_, y_ - 1); }
+	Point getDown() const { return Point(x_, y_ + 1); }
+
+	static double distance(const Point p1, const Point p2)
+	{
+		double dx = (double)p2.x() - (double)p1.x();
+		double dy = (double)p2.y() - (double)p1.y();
+		return sqrt(dx * dx + dy * dy);
+	}
+};
 
 
 class Mass {
@@ -13,27 +47,64 @@ public:
 		WATER,// 進むのが1/3に遅くなる
 		ROAD,//進むのが3倍速い
 	};
+	//リストへ登録しているかを高速に判定する
+	enum listed {
+		NONE,
+	    OPEN,
+		CLOSE,
+	};
 private:
 	status s_ = BLANK;
+	listed listed_ = NONE;
+	Mass* pParent_ = nullptr;
+	Point pos_;
+	int steps_ = 0;
+	double estimate_ = 0.0;
+
+	static int getWalkCostWater(Mass& m) { status s = m.getStatus(); return (s == WATER)  ? 3 : 1; }
+	static int getWalkCostRoad(Mass& m) { status s = m.getStatus(); return (s ==ROAD) ? 1/3 : 1; }
+
+	void calcConst(const Point target)
+	{
+		if (WATER)
+		{
+			steps_ = (pParent_ ? pParent_->steps_ : 0) + getWalkCostWater(*this);
+			estimate_ = Point::distance(pos_, target);
+		}
+		else if (ROAD)
+		{
+			steps_ = (pParent_ ? pParent_->steps_ : 0) +  getWalkCostRoad(*this);
+			estimate_ = Point::distance(pos_, target);
+		}
+		else
+		{
+			steps_ = (pParent_ ? pParent_->steps_ : 0) + 1;
+			estimate_ = Point::distance(pos_, target);
+		}
+	}
 public:
+	Mass (){}
 	void setStatus(status s) { s_ = s; }
 	status getStatus() const { return s_; }
-};
 
-class Point {
-	int x_ = -1,
-		y_ = -1;
-public:
-	Point(int x, int y) :x_(x), y_(y) {}
-	int x() const { return x_; }
-	int y() const { return y_; }
-	void setX(int x) { x_ = x; }
-	void setY(int y) { y_ = y; }
+	void setPos(int x, int y) { pos_.set(x, y); }
+	const Point& getPos()const { return pos_; }
+	int x() { return pos_.x(); }
+	int y() { return pos_.y(); }
 
-	bool operator == (const Point& p) const {
-		return p.x() == x_ && p.y() == y_;}
-	bool operator != (const Point& p) const {
-		return !(p == *this);}
+	void setParent(Mass* pParent, const Point& goal)
+	{
+		pParent_ = pParent; calcConst(goal);
+	}
+	Mass* getParent()
+	{
+		return pParent_;
+	}
+
+	void setListed(listed t) { listed_ = t; }
+	bool isListed(listed t)const { return listed_ == t; }
+
+	double getCost() const { return (double)steps_ + estimate_; }
 };
 
 class Board {
@@ -42,11 +113,15 @@ private:
 		BOARD_SIZE = 10,
 	};
 	Mass mass_[BOARD_SIZE][BOARD_SIZE];
+	Mass& getMass(const Point p) { return mass_[p.y()][p.x()]; }
+
+	std::vector<Mass*>open_list_;
+
 public:
 	Board() {
 		for (int y = 0; y < BOARD_SIZE; y++) {
 			for (int x = 0; x < BOARD_SIZE; x++) {
-				mass_[y][x].setStatus(Mass::BLANK);
+				mass_[y][x].setPos(x, y);
 			}
 		}
 		// 壁
@@ -54,6 +129,7 @@ public:
 		mass_[4][5].setStatus(Mass::WALL);
 		mass_[5][5].setStatus(Mass::WALL);
 		mass_[6][5].setStatus(Mass::WALL);
+
 		// 水
 		for (int y = 4; y <= 7; y++) {
 			for (int x = 1; x <= 4; x++) {
@@ -61,6 +137,7 @@ public:
 			}
 		}
 		// 道
+
 		mass_[4][1].setStatus(Mass::ROAD);
 		mass_[5][1].setStatus(Mass::ROAD);
 		mass_[5][2].setStatus(Mass::ROAD);
@@ -69,9 +146,12 @@ public:
 		mass_[6][2].setStatus(Mass::ROAD);
 		mass_[6][3].setStatus(Mass::ROAD);
 	}
+
+	bool isValidated(const Point& p);
+
 	~Board() {}
 
-	bool find(const Point& start, const Point& goal);
-
+	bool find(const Point& start, const Point& goal);;
+	
 	void show() const;
 };
