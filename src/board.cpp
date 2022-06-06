04e0913e85d86d9d90cccf4f1f204ba4a644e868
nanad <yami8383country@gmail.com>
2022-06-06 14:36:34 +0900

    完成

--- src/board.cpp
+++ src/board.cpp
@@ -1,4 +1,15 @@
+/*
+参考URL
+https://qiita.com/2dgames_jp/items/f29e915357c1decbc4b7
+https://ja.wikipedia.org/wiki/A*
+https://yttm-work.jp/algorithm/algorithm_0015.html
+*/
+
+
 #include "board.h"
+#include <algorithm>
+
+
 
 //pが行ける場所か調べる
 
@@ -6,13 +17,13 @@ bool Board::find(const Point& start, const Point& goal)
 {
 	getMass(start).setStatus(Mass::START);
 	getMass(goal).setStatus(Mass::GOAL);
-
+		
 	Point p = start;
 	while (p != goal) {
-		Mass &m = getMass(p);
+		Mass& m = getMass(p);
 		Mass::status status = m.getStatus();
 		if (p != start) m.setStatus(Mass::WAYPOINT);
-		Point next;
+		Point next = p;
 
 		int diff_x = abs(p.x() - goal.x());//xの差
 		int diff_y = abs(p.y() - goal.y());//yの差
@@ -24,9 +35,102 @@ bool Board::find(const Point& start, const Point& goal)
 		}//↑xが先　　↓yが先
 		if (p.y() < goal.y()) { next.setY(p.y() + 1); if (isValidated(next)) { p = next; continue; } }//上側なら上
 		if (goal.y() < p.y()) { next.setY(p.y() - 1); if (isValidated(next)) { p = next; continue; } }//下側なら下
-	
+
+		if (random_walk(p, next, start))goto next_loop;
+
 		return false;
+
+	next_loop:
+		p = next;
 
 	}
 	return true;
 }
+
+bool Board :: random_walk(const Point& p0, Point& next, const Point& start) {
+	int step = 0;
+	Point p = p0;
+
+	for (int i = 0; i < 100; i++) {//何度か繰り返してダメならあきらめる
+		int step_max = i >> 2;//何度もだめなら歩く歩数を増やす
+
+		int r = rand() / ((RAND_MAX + 1) / 4);
+		next.set(
+			p.x() + ((r == 0) ? 1 : ((r == 1) ? -1 : 0)),
+			p.y() + ((r == 2) ? 1 : ((r == 3) ? -1 : 0)));
+
+		if (isValidated(next)) {
+			if (step_max < ++step)return true;
+			p = next;
+			if (p != start)getMass(p).setStatus(Mass::WAYPOINT);
+		}
+		else {//壁に当たれば最初からやり直し
+			step = 0;
+			p = p0;
+		}
+	}
+	return false;
+}
+
+void Board::Astar() {
+
+}
+
+bool Board::listin(const Point& start, const Point& goal) {
+
+	Mass& mass_start = getMass(start);
+	Mass& mass_goal = getMass(goal);
+	//startとgoalの位置をマスで記憶
+
+	mass_start.setStatus(Mass::START);
+	mass_goal.setStatus(Mass::GOAL);
+	//マスの状態を指示
+
+	//オープンリストに開始ノードを追加
+
+	open_list_.clear();
+	open_list_.push_back(&mass_start);
+	//Vector(Mass*)で作ってる？？
+
+	while (!open_list_.empty())
+	{
+		std :: sort(open_list_.begin(), open_list_.end(), asc);
+		auto it = open_list_.begin();
+
+		Mass* current = *it;
+		if (current->getStatus() == Mass::GOAL) {
+			//目的地であれば経路の完成
+			Mass* p = current;//経路のステータスをMass::WAYPOINTにする
+			while (p) {
+				if (p->getStatus() == Mass::BLANK)
+					p->setStatus(Mass::WAYPOINT); p = p->getParent();
+			}
+			return true;
+		}
+		else {
+			//現在のノードをクローズリストに移す
+			open_list_.erase(it);
+			current->setListed(Mass::CLOSE);
+			//現在のノードのリンs熱する各ノードを調べる
+			const Point& pos = current->getPos();
+			Point next[4] = { pos.getRight(),pos.getLeft(),pos.getUp(),pos.getDown() };
+			for (auto& c : next) {
+				if (c.x() < 0 || BOARD_SIZE <= c.x()) continue;
+				if (c.y() < 0 || BOARD_SIZE <= c.y()) continue;
+				Mass& m = getMass(c);
+
+				//マスに入れられるか↓
+				if (!m.isListed(Mass::OPEN) &&//openリストに含まれていない
+					!m.isListed(Mass::CLOSE) &&//クローズドリストに含まれない
+					m.getStatus() != Mass::WALL) {//障害物でない
+					//オープンリストに移してコストを計算する
+					open_list_.push_back(&m);
+					m.setParent(current, start ,goal);//コストも取っている
+					m.setListed(Mass::OPEN);	
+				}
+			}
+		}
+	}
+	return false;
+}
+
--- src/board.h
+++ src/board.h
@@ -1,5 +1,38 @@
 ﻿#pragma once
 #include <iostream>
+#include <vector>
+
+
+
+inline int pow(int num)
+{
+	return num * num;
+}//二乗を返す　何故かusing.systemが使えなかったため作成
+
+class Point {
+	int x_ = -1,
+		y_ = -1;
+public:
+	Point() {}
+	Point(int x, int y) :x_(x), y_(y) {}
+	int x() const { return x_; }
+	int y() const { return y_; }
+	void setX(int x) { x_ = x; }
+	void setY(int y) { y_ = y; }
+	void set(int x, int y) { x_ = x; y_ = y; }
+
+	bool operator == (const Point& p) const {
+		return p.x() == x_ && p.y() == y_;
+	}
+	bool operator != (const Point& p) const {
+		return !(p == *this);
+	}
+
+	Point getRight()const { return Point(x_ + 1, y_); }
+	Point getLeft()const { return Point(x_ - 1, y_); }
+	Point getUp()const { return Point(x_, y_ - 1); }
+	Point getDown()const { return Point(x_, y_ + 1); }
+};
 
 
 class Mass {
@@ -13,28 +46,61 @@ public:
 		WATER,// 進むのが1/3に遅くなる
 		ROAD,//進むのが3倍速い
 	};
+	enum listed {
+		NONE, 
+		OPEN, 
+		CLOSE,
+	};
 private:
 	status s_ = BLANK;
+	listed listed_ = NONE;
+	Point pos_;
+	Mass* pParent_ = nullptr;
+	int steps_ = 0;
+	double estimate_ = 0.0;
+
+	void calcCost(Mass* pParent , const Point& start, const Point target) {
+		steps_ = (pParent_ ? pParent_->steps_ : 0) + 1;
+		const Point& pos = pParent->getPos();
+		estimate_ = WhatCost(pos, start, target);
+	}
 public:
+	Mass() {}
+	~Mass() {}
 	void setStatus(status s) { s_ = s; }
 	status getStatus() const { return s_; }
+
+	void setPos(int x, int y) { pos_.set(x, y); }
+	const Point& getPos()const { return pos_; }
+	int x() { return pos_.x(); }
+	int y() { return pos_.y(); }
+
+	void setParent(Mass* pParent, const Point& start, const Point& goal) { pParent_ = pParent; calcCost(pParent,start,goal); }
+	Mass* getParent() { return pParent_; }
+
+	void setListed(listed t) { listed_ = t; }
+	bool isListed(listed t)const { return listed_ == t; }
+
+	double getCost()const { return (double)steps_ + estimate_; }
+	//↑estimate が　負荷コスト
+
+	float WhatCost(const Point& p, const Point& start, const Point& goal){
+		float C = abs(start.y() - p.y()) + abs(start.x() - p.x());//実コスト　　現在マスからの移動コスト
+		float H = sqrt(pow(abs(goal.y() - p.y())) + pow(abs(goal.x() - p.x())));//推定コスト　ゴールマスへの予測コスト
+		float M;// マスコスト　マス自体のコスト
+		if (getStatus() == Mass::ROAD)M = 1/3;
+		else if (getStatus() == Mass::WATER)M = 3;
+
+		return (C + H) * M;//合計スコア
+	}
+
 };
 
-class Point {
-	int x_ = -1,
-		y_ = -1;
-public:
-	Point(int x, int y) :x_(x), y_(y) {}
-	int x() const { return x_; }
-	int y() const { return y_; }
-	void setX(int x) { x_ = x; }
-	void setY(int y) { y_ = y; }
 
-	bool operator == (const Point& p) const {
-		return p.x() == x_ && p.y() == y_;}
-	bool operator != (const Point& p) const {
-		return !(p == *this);}
-};
+
+inline bool asc(const Mass* o1, const Mass* o2) {
+	return o1 -> getCost() < o2 -> getCost();
+}
 
 class Board {
 private:
@@ -46,6 +112,9 @@ private:
 	//getMass（Getを生成　Mass型関数　
 	//ポイントの引き数をもらって配列（座標）を返す）
 	Mass& getMass(const Point p) { return mass_[p.y()][p.x()]; }
+
+	std::vector<Mass*> open_list_;
+	
 public:
 	Board() {
 		for (int y = 0; y < BOARD_SIZE; y++) {
@@ -82,9 +151,8 @@ public:
 		}
 		return true;
 	}
-
 	bool find(const Point& start, const Point& goal);
-
+	bool random_walk(const Point& p0, Point& next, const Point& start);
 	void show() const {
 		std::cout << std::endl;
 
@@ -133,5 +201,8 @@ public:
 
 	}
 
+	//A*アルゴリズムの経路探索
+	void Astar();
+	bool listin(const Point& start, const Point& goal);
 
 };
